0e205917518563c4eae43d1740c0f3cb36a77de6
NguyenThiKimToaEm/B1909905 <emb1909905@student.ctu.edu.vn>
2022-09-21 08:42:54 +0700

    cai dat du an

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,30 +1,4 @@
-// exports.create = (req, res) => {
-//   res.send({ message: "create handler" });
-// };
-// exports.findAll = (req, res) => {
-//   res.send({ message: "findAll handler" });
-// };
-
-// exports.findOne = (req, res) => {
-//   res.send({ message: "findOne handler" });
-// };
-
-// exports.update = (req, res) => {
-//   res.send({ message: "update handler" });
-// };
-
-// exports.delete = (req, res) => {
-//   res.send({ message: "delete handler" });
-// };
-
-// exports.deleteALL = (req, res) => {
-//   res.send({ message: "deleteAll handler" });
-// };
-
-// exports.findAllFavorite = (req, res) => {
-//   res.send({ message: "findAllFavorite handler" });
-// };
-
+/* lab1 */
 exports.create = (req, res) => {
   res.send({ message: "create handler" });
 };
@@ -46,3 +20,22 @@ exports.deleteAll = (req, res) => {
 exports.findAllFavorite = (req, res) => {
   res.send({ message: "findAllFavorite handler" });
 };
+/* lab2 */
+// const ApiError = require("../api-error");
+// const ContactService = require("../services/contact.service");
+// const MongoDB = require("../utils/mongodb.util");
+
+// exports.create = async (req, res, next) => {
+//   if (!req.body?.name) {
+//     return next(new ApiError(400, "Name can not be empty"));
+//   }
+//   try {
+//     const ContactService = new ContactService(MongoDB.client);
+//     const document = await ContactService.create(req.body);
+//     return res.send(document);
+//   } catch (error) {
+//     return next(
+//       new ApiError(500, "An error occurred while creating the contact")
+//     );
+//   }
+// };
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*****LAB1****/
 const app = require("./app");
 const config = require("./app/config");
 
@@ -6,3 +7,25 @@ const PORT = config.app.port;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+/* Lab 2 */
+
+// const app = require("./app");
+// const config = require("./app/config");
+// const MongoDB = require("./app/utils/mongodb.util");
+
+// async function startServer() {
+//   try {
+//     await MongoDB.connect(config.db.uri);
+//     console.log("Connect to the database!");
+
+//     const PORT = config.app.port;
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Cannot nonnect to the databast!", error);
+//     process.exit();
+//   }
+// }
+
+// startServer();
