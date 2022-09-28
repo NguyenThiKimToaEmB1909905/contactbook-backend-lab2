58972534a9e8a8561f2580fc19149c4e6a7ad959
NguyenThiKimToaEm/B1909905 <emb1909905@student.ctu.edu.vn>
2022-09-28 11:04:32 +0700

    Cai dat du an

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 /*****LAB1****/
-const app = require("./app");
-const config = require("./app/config");
-
-// start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-/* Lab 2 */
-
 // const app = require("./app");
 // const config = require("./app/config");
-// const MongoDB = require("./app/utils/mongodb.util");
 
-// async function startServer() {
-//   try {
-//     await MongoDB.connect(config.db.uri);
-//     console.log("Connect to the database!");
+// //start server
+// const PORT = config.app.port;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
+/* Lab 2 */
 
-//     const PORT = config.app.port;
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("Cannot nonnect to the databast!", error);
-//     process.exit();
-//   }
-// }
+const app = require("./app");
+const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-// startServer();
+async function startServer() {
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log("Connect to the database!");
+
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Cannot nonnect to the databast!", error);
+    process.exit();
+  }
+}
+
+startServer();
